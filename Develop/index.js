06d588b5fcb0bf411147d721af439dc046b17dad
Kyleah Justice <kikij432@gmail.com>
2022-02-26 00:51:56 -0700

    had to add some more stuff, format some others, hard to explain, it's 1 in the morning.

--- Develop/index.js
+++ Develop/index.js
@@ -1,176 +1,145 @@
 // TODO: Include packages needed for this application
-
-
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-const { generateMarkdown } = require('./utils/generateMarkdown.js')
-const fs = require('fs');
-const inquirer = require('inquirer');
-
-const generateMarkdown = require('../../utils/generateMarkdown.js/generateMarkdown');
+const { generateMarkdown } = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is your project name? (Required)',
-        validate: titleInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('Uh oh! Please enter your title!');
-                return false;
+
+const promptProject = () => {
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your project name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Uh oh! Please enter your project name!');
+                    return false;
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        name: 'githubUsername',
-        message: 'What is your GitHub Username? (Required)',
-        validate: githubInput => {
-            if (githubInput) {
-                return true;
-            } else {
-                console.log('Please enter your GitHub username!');
-                return false;
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Write something here about your project. Like your motivation or a description. (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter something about your project!');
+                    return false;
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email address? (Required)',
-        validate: githubInput => {
-            if (githubInput) {
-                return true;
-            } else {
-                console.log('Please enter your email address');
-                return false;
+        },
+        {
+            type: 'input',
+            name: 'githubUsername',
+            message: 'What is your GitHub Username? (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub username!');
+                    return false;
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        name: 'what',
-        message: 'What kind of project is it and what problem(s) will it help solve? (Required)',
-        validate: whatInput => {
-            if (whatInput) {
-                return true;
-            } else {
-                console.log('Please enter what your project is!');
-                return false;
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address? (Required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email address');
+                    return false;
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        name: 'why',
-        message: 'Why did you decide to create this project? (Required)',
-        validate: whyInput => {
-            if (whyInput) {
-                return true;
-            } else {
-                console.log('Please enter why created this project!');
-                return false;
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please provide installation instructions (step-by-step) for this project. (Required)',
+            validate: installInput => {
+                if (installInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your installation instruction steps!');
+                    return false;
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        name: 'how',
-        message: 'How would someone use this project? (Required)',
-        validate: howInput => {
-            if (howInput) {
-                return true;
-            } else {
-                console.log('Please enter how its used!');
-                return false;
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please provide examples for use. (Required)',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your examples!');
+                    return false;
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Please provide installation instructions (step-by-step) for this project. (Required)',
-        validate: installInput => {
-            if (installInput) {
-                return true;
-            } else {
-                console.log('Please enter your installation instruction steps!');
-                return false;
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Please provide instructions on how one is to test the application. (Required)',
+            validate: testInput => {
+                if (testInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your test instructions!');
+                    return false;
+                }
             }
-        }
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Please provide examples for use. (Required)',
-        validate: usageInput => {
-            if (usageInput) {
-                return true;
-            } else {
-                console.log('Please enter your examples!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'test',
-        message: 'Please provide instructions on how one is to test the application. (Required)',
-        validate: testInput => {
-            if (testInput) {
-                return true;
-            } else {
-                console.log('Please enter your test instructions!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'What license will you use for this project?',
-        choices: ['agpl', 'apache', 'mit', 'no license']
-    },
-];
+        },
+        {
+            type: 'checkbox',
+            name: 'projectLanguages',
+            message: 'What languages did you use to creat this? Select all that apply.',
+            choices: ['CSS','HTML','Node','Javascript','Express','Other']
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What license will you use for this project?',
+            choices: ['agpl', 'apache', 'MIT', 'Open','no license']
+        }   
+    ])
+};
 
 // TODO: Create a function to write README file
-function writeToFile = fileContent => {
-    return new Promise((resolve, reject) => {
-        fs.writeToFile()
-    })
-}(fileName, data) {}
+const writeFile = (fileName, data) => {
+
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('Huzzah! Your file is born!');
+
+    });
+};
 
 // TODO: Create a function to initialize app
 function init() {
-    return inquirer.prompt(questions)
-    .then(readmeData => {
-        return readmeData;
-    })
+    promptProject()
+        .then(input => {
+            return generateMarkdown(input);
+        })
+        .then(markdown => {
+            writeFile('readMeDemo.md', markdown);
+        })
+        .catch(err => {
+            console.log(err);
+        })
 }
-
-// Function call to initialize app
-init()
-.then(readmeData => {
-    console.log(readmeData);
-    return generateMarkdown(readmeData);
-})
-.then(pageMD => {
-    return writeToFile(pageMD);
-})
-.then(writeFileResponse => {
-    console.log(writeToFileResponse.message);
-})
-.catch(err => {
-    console.log(error);
-})
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
 
 // Function call to initialize app
 init();
